e75e12d947031eb24904de9525756d7f620b61b4
William Sarti Jose <william.jose@totvs.com.br>
2022-07-06 21:29:08 -0300

    Criando classe - Problema Produtos - Finalizando o programa

--- application/src/application/Program.java
+++ application/src/application/Program.java
@@ -11,8 +11,7 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		
+
 		Product product = new Product();
 		System.out.println("Enter product data: ");
 		System.out.println("Name:  ");
@@ -22,14 +21,20 @@ public class Program {
 		System.out.print("Quantity in stock: ");
 		product.quantity = sc.nextInt();
 
-		
-		System.out.println(product );
-		
-		
-		
-		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		System.out.println();
+		System.out.println("Updated data: " + product);
 		sc.close();
-
 	}
-
 }
