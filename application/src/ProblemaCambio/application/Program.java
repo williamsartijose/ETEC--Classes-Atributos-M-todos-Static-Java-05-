7010d4f48ae264a94cd251deeeeb92723064a9ca
William Sarti Jose <william.jose@totvs.com.br>
2022-07-09 00:29:48 -0300

    PARTE 1: Classes, atributos, métodos - (Problema cambio)

---
+++ application/src/ProblemaCambio/application/Program.java
@@ -0,0 +1,21 @@
+package ProblemaCambio.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import ProblemaCambio.entities.CurrencyConverter;
+
+
+public class Program {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Qual o preço do dólar? ");
+		double dollarPrice = sc.nextDouble();
+		System.out.print("Quantos dólares serão comprados? ");
+		double amount = sc.nextDouble();
+		double result = CurrencyConverter.dollarToReal(amount, dollarPrice);
+		System.out.printf("Valor a ser pago em reais = %.2f%n", result);
+		sc.close();
+	}
+}
